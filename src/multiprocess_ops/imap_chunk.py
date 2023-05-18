56ad8af6fe68816ac6ebd090ec7d031ca174a209
Chiawei <33477318+codenamewei@users.noreply.github.com>
2023-05-18 13:34:20 +0900

    Update and rename imap_chunk_main.py to imap_chunk.py

--- src/multiprocess_ops/imap_chunk.py
+++ src/multiprocess_ops/imap_chunk.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     # create and configure the process pool
     with Pool(4) as pool:
         # issue tasks to the process pool
-        #results = pool.imap(task, range(20), chunksize=10)
+        results = pool.imap(task, range(20), chunksize=10)
         #results = pool.imap_unordered(task, range(40), chunksize=10)
         
         # shutdown the process pool
